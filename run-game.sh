c0390b5ee648804e76887876f1aa6ac7e8535999
viggo1 <vignie1217@skola.goteborg.se>
2024-10-29 10:40:18 +0100

    added code to get the script working

--- run-game.sh
+++ run-game.sh
@@ -1,4 +1,14 @@
-#!bin/bash/
-
+#!/bin/bash
+echo "Viggos program"
 mkdir -p "$USER"_Labb
-
+cp *.java "$USER"_Labb
+cd "$USER"_Labb
+pwd
+echo "compiling java files..." 
+javac GuessingGame.java
+echo "starting game" 
+java GuessingGame.java
+echo "Done"
+rm -r GuessingGame.class Guesser.class
+echo "deleted class files created when compiling"
+ls 
